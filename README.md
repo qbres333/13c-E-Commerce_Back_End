162a5422bf139c3d4239fdcfa282dcbb5dce11dd
Fin M <qbres333@hotmail.com>
2024-09-26 17:43:47 -0600

    add category and product columns

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 13c-E-Commerce_Back_End
+
+## Credits
+
+Sequelize data types:
+https://sequelize.org/docs/v7/models/data-types/
--- models/Category.js
+++ models/Category.js
@@ -7,13 +7,24 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    //id, category_name Mod 13 Act 23
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
