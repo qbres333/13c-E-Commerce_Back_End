afda8dbbc3edf70496179e2004ba8de5164e4d7f
Fin M <qbres333@hotmail.com>
2024-09-27 16:36:42 -0600

    added delete routes for product and tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -124,6 +124,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    // return an error if id not found
+    if (!productData) {
+      res.status(404).json({ message: "No product found with that id!" });
+    }
+    res.status(200).json(category_data);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
+
 // The `/api/tags` endpoint
 
 // similar to category_routes file
@@ -69,7 +70,19 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
   try {
-  } catch (err) {}
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    // return an error if id not found
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with that id!" });
+    }
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
